2d47f1ce1c505573a4e1da682c6b818c2a4d726f
Greg Izzy <izydorek.g@gmail.com>
2017-07-26 20:03:38 +0200

    git init

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//jscripts.9.4
+
+function drawTree(n) {
+  for ( var i = 0 ; i < n ; i++ ) {
+    var star = "";
+    for (var j = 0 ; j <= i ; j++ ) {
+      star += "*";
+      }
+    console.log(star);
+    }
+}
+drawTree(5)
